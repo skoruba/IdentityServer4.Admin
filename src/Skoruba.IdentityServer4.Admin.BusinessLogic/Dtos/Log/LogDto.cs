107cd8799f7ffd0b6a3e4ce86a9e54797fddd19d
Jan Skoruba <jan@skoruba.com>
2018-07-08 15:39:07 +0200

    Add missing log files

---
+++ src/Skoruba.IdentityServer4.Admin.BusinessLogic/Dtos/Log/LogDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Skoruba.IdentityServer4.Admin.BusinessLogic.Dtos.Log
+{
+    public class LogDto
+    {
+        public long Id { get; set; }
+
+        public string Message { get; set; }
+
+        public string MessageTemplate { get; set; }
+
+        public string Level { get; set; }
+
+        public DateTimeOffset TimeStamp { get; set; }
+
+        public string Exception { get; set; }
+
+        public string LogEvent { get; set; }
+
+        public string Properties { get; set; }
+
+        public XElement PropertiesXml => XElement.Parse(Properties);
+    }
+}
---
+++ src/Skoruba.IdentityServer4.Admin.BusinessLogic/Dtos/Log/LogsDto.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Skoruba.IdentityServer4.Admin.BusinessLogic.Dtos.Log
+{
+    public class LogsDto
+    {
+        public LogsDto()
+        {
+            Logs = new List<LogDto>();
+        }
+
+        public List<LogDto> Logs { get; set; }
+
+        public int TotalCount { get; set; }
+
+        public int PageSize { get; set; }
+    }
+}
