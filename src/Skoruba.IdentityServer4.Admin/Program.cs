be7684978de11db13f64224f5579acd6c13e21ca
Jan Skoruba <jan@skoruba.com>
2018-07-26 09:48:46 +0200

    Add note related to using the seed data

--- src/Skoruba.IdentityServer4.Admin/Program.cs
+++ src/Skoruba.IdentityServer4.Admin/Program.cs
@@ -12,6 +12,7 @@ namespace Skoruba.IdentityServer4.Admin
         {
             var host = BuildWebHost(args);
 
+            //NOTE: Uncomment the line below to use seed data
             //await DbMigrationHelpers.EnsureSeedData(host);
 
             host.Run();
