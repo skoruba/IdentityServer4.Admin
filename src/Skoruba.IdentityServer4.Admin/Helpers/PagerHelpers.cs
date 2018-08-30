a9390c4172bf47ecf7c8f7aa408f9018df77ca63
Jan Skoruba <jan@skoruba.com>
2018-08-30 13:05:44 +0200

    Clean up Pager - #70

--- src/Skoruba.IdentityServer4.Admin/Helpers/PagerHelpers.cs
+++ src/Skoruba.IdentityServer4.Admin/Helpers/PagerHelpers.cs
@@ -9,22 +9,28 @@ namespace Skoruba.IdentityServer4.Admin.Helpers
             return (int)Math.Ceiling((double)totalCount / pageSize);
         }
 
-        public static bool ShowRightPagerButton(int maxPages, int totalPages, string currentPage)
+        public static bool IsActivePage(int currentPage, int currentIteration)
+        {
+            return currentPage == currentIteration;
+        }
+
+        public static bool ShowRightPagerButton(int maxPages, int totalPages, int currentPage)
         {
             var maxPageToRender = GetMaxPageToRender(maxPages, totalPages, currentPage);
 
             return maxPageToRender < totalPages;
         }
 
-        public static bool ShowLeftPagerButton(int maxPages, int totalPages, string currentPage)
+        public static bool ShowLeftPagerButton(int maxPages, int totalPages, int currentPage)
         {
             var minPageToRender = GetMinPageToRender(maxPages, totalPages, currentPage);
 
             return minPageToRender > maxPages;
         }
 
-        public static int GetMinPageToRender(int maxPages, int totalPages, string currentPage)
+        public static int GetMinPageToRender(int maxPages, int totalPages, int currentPage)
         {
+            const int defaultPageNumber = 1;
             var currentMaxPages = GetMaxPageToRender(maxPages, totalPages, currentPage);
 
             if (currentMaxPages < maxPages) return currentMaxPages;
@@ -34,19 +40,18 @@ namespace Skoruba.IdentityServer4.Admin.Helpers
                 currentMaxPages = GetMaxPage(maxPages, totalPages, currentPage);
             }
 
-            var minPageToRender = currentMaxPages - maxPages + 1;
+            var minPageToRender = currentMaxPages - maxPages + defaultPageNumber;
 
             return minPageToRender;
         }
 
-        public static int GetMaxPage(int maxPages, int totalPages, string currentPage)
+        public static int GetMaxPage(int maxPages, int totalPages, int currentPage)
         {
-            var page = GetCurrentPage(currentPage);
-            var result = (int)Math.Ceiling((double)page / maxPages);
+            var result = (int)Math.Ceiling((double)currentPage / maxPages);
             return result * maxPages;
         }
 
-        public static int GetMaxPageToRender(int maxPages, int totalPages, string currentPage)
+        public static int GetMaxPageToRender(int maxPages, int totalPages, int currentPage)
         {
             var currentMaxPages = GetMaxPage(maxPages, totalPages, currentPage);
 
@@ -55,9 +60,13 @@ namespace Skoruba.IdentityServer4.Admin.Helpers
 
         public static int GetCurrentPage(string currentPage)
         {
+            const int defaultPageNumber = 1;
+
             try
             {
-                return string.IsNullOrEmpty(currentPage) ? 1 : Convert.ToInt32(currentPage);
+                return string.IsNullOrEmpty(currentPage)
+                    ? defaultPageNumber
+                    : (Convert.ToInt32(currentPage) <= 0 ? defaultPageNumber : Convert.ToInt32(currentPage));
             }
             catch
             {
