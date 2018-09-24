ffb0138cc12b5a88609c4c0baa6122258bc614bd
janskoruba <jan@skoruba.com>
2018-09-24 18:48:10 +0200

    Rename projects related to rewrite of BL.

--- src/Skoruba.IdentityServer4.Admin.AspNetIdentity/Dtos/Identity/Base/BaseRoleDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Skoruba.IdentityServer4.Admin.AspNetIdentity.Dtos.Identity.Base
-{
-    public class BaseRoleDto<TRoleId>
-    {
-        public TRoleId Id { get; set; }
-    }
-}
--- src/Skoruba.IdentityServer4.Admin.AspNetIdentity/Dtos/Identity/Base/BaseUserChangePasswordDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Skoruba.IdentityServer4.Admin.AspNetIdentity.Dtos.Identity.Base
-{
-    public class BaseUserChangePasswordDto<TUserId>
-    {
-        public TUserId UserId { get; set; }
-    }
-}
