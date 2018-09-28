a9a1a90269686cc907877cb1e6425af4c38620ce
janskoruba <jan@skoruba.com>
2018-09-28 12:13:25 +0200

    Add project template

---
+++ templates/template/content/src/SkorubaIdentityServer4Admin.Admin/Helpers/DbMigrationHelpers.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.EntityFramework.Mappers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SkorubaIdentityServer4Admin.Admin.Configuration.Constants;
+using SkorubaIdentityServer4Admin.Admin.Configuration.Identity;
+using SkorubaIdentityServer4Admin.Admin.Configuration.IdentityServer;
+using SkorubaIdentityServer4Admin.Admin.Configuration.Interfaces;
+using Skoruba.IdentityServer4.Admin.EntityFramework.DbContexts;
+using Skoruba.IdentityServer4.Admin.EntityFramework.Identity.Entities.Identity;
+
+namespace SkorubaIdentityServer4Admin.Admin.Helpers
+{
+    public static class DbMigrationHelpers
+    {
+        /// <summary>
+        /// Generate migrations before running this method, you can use command bellow:
+        /// Nuget package manager: Add-Migration DbInit -context AdminDbContext -output Data/Migrations
+        /// Dotnet CLI: dotnet ef migrations add DbInit -c AdminDbContext -o Data/Migrations
+        /// </summary>
+        /// <param name="host"></param>
+        public static async Task EnsureSeedData(IWebHost host)
+        {
+            using (var serviceScope = host.Services.CreateScope())
+            {
+                var services = serviceScope.ServiceProvider;
+
+                await EnsureSeedData(services);
+            }
+        }
+
+        public static async Task EnsureSeedData(IServiceProvider serviceProvider)
+        {
+            using (var scope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<AdminDbContext>();
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<UserIdentity>>();
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<UserIdentityRole>>();
+
+                var rootConfiguration = scope.ServiceProvider.GetRequiredService<IRootConfiguration>();
+
+                context.Database.Migrate();
+                await EnsureSeedIdentityServerData(context, rootConfiguration.AdminConfiguration);
+                await EnsureSeedIdentityData(userManager, roleManager);
+            }
+        }
+
+        /// <summary>
+        /// Generate default admin user / role
+        /// </summary>
+        private static async Task EnsureSeedIdentityData(UserManager<UserIdentity> userManager,
+            RoleManager<UserIdentityRole> roleManager)
+        {
+            // Create admin role
+            if (!await roleManager.RoleExistsAsync(AuthorizationConsts.AdministrationRole))
+            {
+                var role = new UserIdentityRole { Name = AuthorizationConsts.AdministrationRole };
+
+                await roleManager.CreateAsync(role);
+            }
+
+            // Create admin user
+            if (await userManager.FindByNameAsync(Users.AdminUserName) != null) return;
+
+            var user = new UserIdentity
+            {
+                UserName = Users.AdminUserName,
+                Email = Users.AdminEmail,
+                EmailConfirmed = true
+            };
+
+            var result = await userManager.CreateAsync(user, Users.AdminPassword);
+
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(user, AuthorizationConsts.AdministrationRole);
+            }
+        }
+
+        /// <summary>
+        /// Generate default clients, identity and api resources
+        /// </summary>
+        private static async Task EnsureSeedIdentityServerData(AdminDbContext context, IAdminConfiguration adminConfiguration)
+        {
+            if (!context.Clients.Any())
+            {
+                foreach (var client in Clients.GetAdminClient(adminConfiguration).ToList())
+                {
+                    await context.Clients.AddAsync(client.ToEntity());
+                }
+
+                await context.SaveChangesAsync();
+            }
+
+            if (!context.IdentityResources.Any())
+            {
+                var identityResources = ClientResources.GetIdentityResources().ToList();
+
+                foreach (var resource in identityResources)
+                {
+                    await context.IdentityResources.AddAsync(resource.ToEntity());
+                }
+
+                await context.SaveChangesAsync();
+            }
+
+            if (!context.ApiResources.Any())
+            {
+                foreach (var resource in ClientResources.GetApiResources().ToList())
+                {
+                    await context.ApiResources.AddAsync(resource.ToEntity());
+                }
+
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ templates/template/content/src/SkorubaIdentityServer4Admin.Admin/Scripts/App/components/Picker.js
@@ -0,0 +1,223 @@
+﻿ko.components.register('picker', {
+	viewModel: function (params) {
+
+		var self = this;
+
+		//Constants
+		const minSearchSelectConst = 2;
+		const inputSearchDelay = 500;
+		const topSuggestedItemsConst = 5;
+
+		//Input
+		this.textTerm = ko.observable("").extend({ rateLimit: inputSearchDelay });
+		this.minSearchText = ko.observable(params.minSearchText || minSearchSelectConst);
+		this.multipleSelect = ko.observable(params.multipleSelect || false);
+
+		//Labels
+		this.searchInputPlaceholder = ko.observable(params.searchInputPlaceholder || `Enter ${this.minSearchText()} or more characters`);
+		this.selectedItemsTitle = ko.observable(params.selectedItemsTitle || "Selected: ");
+		this.searchResultTitle = ko.observable(params.searchResultTitle || "Search result: ");
+		this.suggestedItemsTitle = ko.observable(params.suggestedItemsTitle || "Suggested items: ");
+
+		this.noItemSelectedTitle = ko.observable(params.noItemSelectedTitle || "No item/s selected");
+		this.showAllItemsTitle = ko.observable(params.showAllItemsTitle || "more");
+		this.allowSuggestedItems = ko.observable((params.allowSuggestedItems && params.url) || false);
+		this.topSuggestedItems = ko.observable(params.topSuggestedItems || topSuggestedItemsConst);
+		this.allowItemAlreadySelectedNotification = ko.observable(params.allowItemAlreadySelectedNotification || true);
+		this.itemAlreadySelectedTitle = ko.observable(params.itemAlreadySelectedTitle || "item already selected");
+
+		//Collections
+		this.searchResult = ko.observableArray([]);
+		this.selectedResult = ko.observableArray(params.selectedItems || []);
+		this.suggestedResult = ko.observableArray([]);
+
+		//Features
+		this.loading = ko.observable(false);
+
+		//Sync selected items to hiddenField for server-side
+		const selectedItems = ko.toJSON(this.selectedResult);
+		if (this.multipleSelect() === true) {
+			if (this.selectedResult().length === 0) {
+				$(`#${params.hiddenId}`).val("");
+			} else {
+				$(`#${params.hiddenId}`).val(selectedItems);
+			}
+		} else {
+			if (this.selectedResult().length === 0) {
+				$(`#${params.hiddenId}`).val("");
+			} else {
+				$(`#${params.hiddenId}`).val(this.selectedResult()[0]);
+			}
+		}
+
+		//Track changes on search input
+		this.textTerm.subscribe(function (searchTerm) {
+
+			//If is search input clear -> clear search result
+			if (searchTerm.trim() === "") {
+				self.searchResult([]);
+			}
+
+			//If search term isn't empty and has min length characters
+			if (searchTerm.trim() !== "" && searchTerm.trim().length >= self.minSearchText()) {
+
+				if (params.url) {
+					//start loading
+					self.loading(true);
+
+					//make ajax request and result add to search result
+					$.get(`${params.url}=${searchTerm}`,
+						function (data) {
+
+							if (data.indexOf(searchTerm) === -1) {
+								data.push(searchTerm);
+							}
+
+							self.searchResult(data);
+
+							//stop loading
+							self.loading(false);
+						});
+				} else {
+					self.searchResult([searchTerm]);
+				}
+			}
+		});
+
+		this.notify = function (item) {
+			toastr.options.closeButton = true;
+			toastr.options.preventDuplicates = true;
+			toastr.info(`${item} ${this.itemAlreadySelectedTitle()}`);
+		}
+
+		//Action methods
+		this.add = function (item) {
+
+			//Replace quotes
+			item = item.replace(/'/g, "").replace(/"/g, "");
+
+			//Check if selected item is exists in selected items array
+			if (this.selectedResult.indexOf(item) > -1) {
+
+				if (this.allowItemAlreadySelectedNotification() === true) {
+					this.notify(item);
+				}
+
+				return;
+			}
+
+			//Single select -> the original value replace with new
+			if (this.multipleSelect() === false) {
+				this.selectedResult([]);
+				this.selectedResult.push(item);
+				this.clear();
+				this.sync();
+			}
+			//Multiple select
+			else if (this.multipleSelect() === true) {
+				this.selectedResult.push(item);
+				this.clear();
+				this.sync();
+			}
+		}
+
+		//Get suggested items - by default 5 items
+		this.getSuggestedItems = function () {
+
+			if (self.allowSuggestedItems() === false) return;
+
+			if (params.url) {
+				//start loading
+				self.loading(true);
+
+				//make ajax request and result add to suggested result
+				$.get(params.url,
+					{
+						limit: self.topSuggestedItems()
+					},
+					function (data) {
+
+						self.suggestedResult(data);
+
+						//stop loading
+						self.loading(false);
+					});
+			}
+		}
+
+		//Clear search input and search result
+		this.clear = function () {
+			this.textTerm("");
+			self.searchResult([]);
+		}
+
+		//Remove selected item
+		this.remove = function (item) {
+			this.selectedResult.remove(item);
+			this.sync();
+		}
+
+		//Show all items into suggested items
+		this.showAll = function () {
+
+			if (params.url) {
+				//start loading
+				self.loading(true);
+
+				//make ajax request and result add to search result
+				$.get(`${params.url}`,
+					function (data) {
+
+						self.suggestedResult(data);
+
+						//stop loading
+						self.loading(false);
+					});
+			}
+		}
+
+		//Synchronize the selected items to hidden field for server-side part
+		this.sync = function () {
+			const selectedItems = ko.toJSON(this.selectedResult);
+			if (this.multipleSelect() === true) {
+				if (this.selectedResult().length === 0) {
+					$(`#${params.hiddenId}`).val("");
+				} else {
+					$(`#${params.hiddenId}`).val(selectedItems);
+				}
+			} else {
+				if (this.selectedResult().length === 0) {
+					$(`#${params.hiddenId}`).val("");
+				} else {
+					$(`#${params.hiddenId}`).val(this.selectedResult()[0]);
+				}
+			}
+		}
+
+		//Load suggested items
+		self.getSuggestedItems();
+	},
+	template:
+		'<div class="card">' +
+		'<div class="card-body">' +
+		'<input class="form-control" data-bind="textInput: textTerm, attr: {placeholder: searchInputPlaceholder}" />' +
+		'<hr data-bind="visible: searchResult().length > 0" />' +
+		'<div data-bind="visible: searchResult().length > 0, text: searchResultTitle" class="search-title"></div>' +
+		'<div class="block__buttons__add" data-bind="foreach: searchResult"><input type="button" class="btn btn-primary button__add" data-bind="value: $data, click: function() { $parent.add($data); }"></div>' +
+		'<img data-bind="visible: loading()" src="/images/loading.gif" alt="Loading.." />' +
+		'<hr />' +
+		'<div data-bind="visible: selectedResult().length == 0, text: noItemSelectedTitle" class="search-title"></div>' +
+		'<div data-bind="visible: selectedResult().length > 0, text: selectedItemsTitle" class="search-title"></div>' +
+		'<div data-bind="foreach: selectedResult"><button class="btn btn-outline-primary button__delete" data-bind="click: function() { $parent.remove($data); }"><span data-bind="text: $data"></span> <span class="oi oi-x"></span></button></div>' +
+
+		'<hr data-bind="visible: suggestedResult().length > 0" />' +
+		'<div data-bind="visible: suggestedResult().length > 0, text: suggestedItemsTitle" class="search-title"></div>' +
+		'<!-- ko foreach: suggestedResult -->' +
+		'<input type="button" class="btn btn-light button__add" data-bind="value: $data, click: function() { $parent.add($data); }">' +
+		'<!-- /ko -->' +
+		'<button class="btn btn-default button__show-all" data-bind="visible:allowSuggestedItems(), click: function() { $component.showAll(); }"><span data-bind="text: showAllItemsTitle"></span> <span class="oi oi-plus"></span></button>' +
+		'</div>' +
+		'</div>'
+});
+
+ko.applyBindings();
