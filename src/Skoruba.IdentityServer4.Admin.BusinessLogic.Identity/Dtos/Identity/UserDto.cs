6c1aca5e6d1a706858bf880929d3d0c51dcde7e4
Michał Drzał <michal.drzal@gmail.com>
2018-10-08 17:27:11 +0200

    Changed regex to match default allowed usernames
    https://github.com/aspnet/Identity/blob/c7276ce2f76312ddd7fccad6e399da96b9f6fae1/src/Core/UserOptions.cs

--- src/Skoruba.IdentityServer4.Admin.BusinessLogic.Identity/Dtos/Identity/UserDto.cs
+++ src/Skoruba.IdentityServer4.Admin.BusinessLogic.Identity/Dtos/Identity/UserDto.cs
@@ -7,7 +7,7 @@ namespace Skoruba.IdentityServer4.Admin.BusinessLogic.Identity.Dtos.Identity
     public class UserDto<TKey> : BaseUserDto<TKey>
     {        
         [Required]
-        [RegularExpression(@"^\S+$")]
+        [RegularExpression(@"^[a-zA-Z0-9_@\-\.\+]+$")]
         public string UserName { get; set; }
 
         [Required]
