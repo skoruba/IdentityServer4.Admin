5fd4aef33149d541b80fc6765585f946c6ec2ef6
janskoruba <jan@skoruba.com>
2018-10-21 12:56:51 +0200

    Remove external providers for testing

--- src/Skoruba.IdentityServer4.AspNetIdentity/Startup.cs
+++ src/Skoruba.IdentityServer4.AspNetIdentity/Startup.cs
@@ -84,31 +84,7 @@ namespace Skoruba.IdentityServer4.AspNetIdentity
                     options.TokenCleanupInterval = 15; // frequency in seconds to cleanup stale grants. 15 is useful during debugging
 #endif                
                 });
-
-
-            services.AddAuthentication()
-                .AddGoogle("Google", options =>
-                {
-                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-
-                    options.ClientId = "434483408261-55tc8n0cs4ff1fe21ea8df2o443v2iuc.apps.googleusercontent.com";
-                    options.ClientSecret = "3gcoTrEDPPJ0ukn_aYYT6PWo";
-                })
-                .AddOpenIdConnect("oidc", "OpenID Connect", options =>
-                {
-                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-                    options.SignOutScheme = IdentityServerConstants.SignoutScheme;
-
-                    options.Authority = "https://demo.identityserver.io/";
-                    options.ClientId = "implicit";
-
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        NameClaimType = "name",
-                        RoleClaimType = "role"
-                    };
-                });
-
+          
             if (Environment.IsDevelopment())
             {
                 builder.AddDeveloperSigningCredential();
