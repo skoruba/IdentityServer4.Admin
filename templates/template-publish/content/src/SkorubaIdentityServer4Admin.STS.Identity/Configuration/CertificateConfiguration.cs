9f8614be7c42680a730e6546a8369163852b9915
janskoruba <jan@skoruba.com>
2018-12-30 16:19:23 +0100

    Update project template for dotnet CLI

---
+++ templates/template-publish/content/src/SkorubaIdentityServer4Admin.STS.Identity/Configuration/CertificateConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace SkorubaIdentityServer4Admin.STS.Identity.Configuration
+{
+	public class CertificateConfiguration
+	{
+		public bool UseTemporaryKey { get; set; }
+
+		public string SigningCertificateThumbprint { get; set; }
+
+		public string ValidationCertificateThumbprint { get; set; }
+	}
+}
