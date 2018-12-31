65de34ce002a186f1269388ace3ae6194ab06d2e
janskoruba <jan@skoruba.com>
2018-12-31 15:34:58 +0100

    Add notice about modification and add source of file. Fix namespace in device folder.

--- src/Skoruba.IdentityServer4.STS.Identity/Quickstart/Account/ExternalController.cs
+++ src/Skoruba.IdentityServer4.STS.Identity/Quickstart/Account/ExternalController.cs
@@ -1,3 +1,9 @@
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+// Original file: https://github.com/IdentityServer/IdentityServer4.Samples
+// Modified by Jan Škoruba
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- src/Skoruba.IdentityServer4.STS.Identity/Quickstart/Extensions.cs
+++ src/Skoruba.IdentityServer4.STS.Identity/Quickstart/Extensions.cs
@@ -1,3 +1,9 @@
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+// Original file: https://github.com/IdentityServer/IdentityServer4.Quickstart.UI
+// Modified by Jan Škoruba
+
 using System.Threading.Tasks;
 using IdentityServer4.Stores;
 
