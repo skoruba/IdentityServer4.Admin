4512142ee297b7d49521c4e49e7a343f26955b06
SNS <sns@kmnet.net>
2019-01-12 00:50:12 +0800

    Fix carsh on SQL Server 2008 R2

--- src/Skoruba.IdentityServer4.Admin/Helpers/StartupHelpers.cs
+++ src/Skoruba.IdentityServer4.Admin/Helpers/StartupHelpers.cs
@@ -44,7 +44,7 @@ namespace Skoruba.IdentityServer4.Admin.Helpers
             var storeOptions = new ConfigurationStoreOptions();
             services.AddSingleton(storeOptions);
 
-            services.AddDbContext<TContext>(options => options.UseSqlServer(configuration.GetConnectionString(ConfigurationConsts.AdminConnectionStringKey), optionsSql => optionsSql.MigrationsAssembly(migrationsAssembly)));
+            services.AddDbContext<TContext>(options => options.UseSqlServer(configuration.GetConnectionString(ConfigurationConsts.AdminConnectionStringKey), optionsSql => optionsSql.MigrationsAssembly(migrationsAssembly).UseRowNumberForPaging()));
         }
 
         public static void RegisterDbContextsStaging<TContext>(this IServiceCollection services)
