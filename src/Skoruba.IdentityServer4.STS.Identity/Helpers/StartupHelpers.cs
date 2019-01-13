39b4999b7115c708251ce35bfb8b661f652735f5
janskoruba <jan@skoruba.com>
2019-01-13 11:46:30 +0100

    Add security headers

--- src/Skoruba.IdentityServer4.STS.Identity/Helpers/StartupHelpers.cs
+++ src/Skoruba.IdentityServer4.STS.Identity/Helpers/StartupHelpers.cs
@@ -55,6 +55,9 @@ namespace Skoruba.IdentityServer4.STS.Identity.Helpers
             {
                 ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
             });
+
+            app.UseXfo(options => options.SameOrigin());
+            app.UseReferrerPolicy(options => options.NoReferrer());
         }
 
         public static void AddAuthenticationServices<TContext, TUserIdentity, TUserIdentityRole>(this IServiceCollection services, IHostingEnvironment hostingEnvironment, IConfiguration configuration, ILogger logger) where TContext : DbContext
