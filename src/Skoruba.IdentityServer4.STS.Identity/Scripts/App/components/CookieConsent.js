dc202e53a73ea8d2a6159501aa7195bda7cbc31f
janskoruba <jan@skoruba.com>
2019-02-12 17:03:27 +0100

    Fix cookieconsent palette - use CSS instead of JS settings.

--- src/Skoruba.IdentityServer4.STS.Identity/Scripts/App/components/CookieConsent.js
+++ src/Skoruba.IdentityServer4.STS.Identity/Scripts/App/components/CookieConsent.js
@@ -1,18 +1,8 @@
 ﻿window.addEventListener("load", function () {
     window.cookieconsent.initialise({
-        "palette": {
-            "popup": {
-                "background": "#eaf7f7",
-                "text": "#5c7291"
-            },
-            "button": {
-                "background": "#56cbdb",
-                "text": "#ffffff"
-            }
-        },
-        "position": "bottom-right",
+       "position": "bottom-right",
         "content": {
-            "message": "This website uses cookies to keep you logged in and ensure that you get the best experience on our website. Your personal data never leaves this website until you explicitly allow for it in consent screen.",
+            "message": "This website uses cookies to keep you logged in and ensure that you get the best experience on our website. Your personal data never leaves this website until you explicitly allow for it in consent screen."
         }
     });
 });
