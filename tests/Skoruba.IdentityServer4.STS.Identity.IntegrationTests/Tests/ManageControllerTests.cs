2b34453d21bc969dcf24a2d4206a904e9d16a059
janskoruba <jan@skoruba.com>
2019-03-06 15:28:55 +0100

    Move UserIsAbleToUpdateProfile test to ManageControllerTests

--- tests/Skoruba.IdentityServer4.STS.Identity.IntegrationTests/Tests/ManageControllerTests.cs
+++ tests/Skoruba.IdentityServer4.STS.Identity.IntegrationTests/Tests/ManageControllerTests.cs
@@ -59,5 +59,37 @@ namespace Skoruba.IdentityServer4.STS.Identity.IntegrationTests.Tests
                 response.Headers.Location.ToString().Should().Contain("Account/Login");
             }
         }
+        
+        [Fact]
+        public async Task UserIsAbleToUpdateProfile()
+        {
+            // Clear headers
+            _client.DefaultRequestHeaders.Clear();
+
+            // Register new user
+            var registerFormData = UserMocks.GenerateRegisterData();
+            var registerResponse = await UserMocks.RegisterNewUserAsync(_client, registerFormData);
+
+            // Get cookie with user identity for next request
+            _client.PutCookiesOnRequest(registerResponse);
+
+            // Prepare request to update profile
+            const string manageAction = "/Manage/Index";
+            var manageResponse = await _client.GetAsync(manageAction);
+            var antiForgeryToken = await manageResponse.ExtractAntiForgeryToken();
+
+            var manageProfileData = UserMocks.GenerateManageProfileData(registerFormData["Email"], antiForgeryToken);
+
+            // Update profile
+            var requestWithAntiForgeryCookie = PostRequestHelper.CreateWithCookiesFromResponse(manageAction, manageProfileData, manageResponse);
+            var requestWithIdentityCookie = CookiesHelper.CopyCookiesFromResponse(requestWithAntiForgeryCookie, registerResponse);
+            var responseMessage = await _client.SendAsync(requestWithIdentityCookie);
+
+            // Assert      
+            responseMessage.StatusCode.Should().Be(HttpStatusCode.Redirect);
+
+            //The redirect to login
+            responseMessage.Headers.Location.ToString().Should().Be("/Manage");
+        }
     }
 }
