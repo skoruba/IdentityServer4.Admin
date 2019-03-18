a530cf7559a8118d605f50c12caf9318b9384d14
brice <brice@xcit.org>
2019-03-18 21:20:41 +0100

    Removed unused code

--- src/Skoruba.IdentityServer4.Admin/Configuration/Constants/ConfigurationConsts.cs
+++ src/Skoruba.IdentityServer4.Admin/Configuration/Constants/ConfigurationConsts.cs
@@ -6,14 +6,6 @@ namespace Skoruba.IdentityServer4.Admin.Configuration.Constants
     {
         [Obsolete] public const string AdminConnectionStringKey = "AdminConnection";
 
-		[Obsolete] public const string ConfigurationDbConnectionStringKey = "ConfigurationDbConnection";
-
-		[Obsolete] public const string PersistedGrantDbConnectionStringKey = "PersistedGrantDbConnection";
-
-		[Obsolete] public const string IdentityDbConnectionStringKey = "IdentityDbConnection";
-
-		[Obsolete] public const string AdminLogDbConnectionStringKey = "AdminLogDbConnection";
-
         public const string ResourcesPath = "Resources";
 
         public const string AdminConfigurationKey = "AdminConfiguration";
