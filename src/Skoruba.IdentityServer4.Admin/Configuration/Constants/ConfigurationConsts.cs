a2e964666d42d40c5b561d19557da52a7f9097e9
brice <brice@xcit.org>
2019-03-18 21:27:43 +0100

    Removed unused constants

--- src/Skoruba.IdentityServer4.Admin/Configuration/Constants/ConfigurationConsts.cs
+++ src/Skoruba.IdentityServer4.Admin/Configuration/Constants/ConfigurationConsts.cs
@@ -4,10 +4,6 @@ namespace Skoruba.IdentityServer4.Admin.Configuration.Constants
 {
     public class ConfigurationConsts
     {
-        [Obsolete] public const string AdminConnectionStringKey = "AdminConnection";
-
         public const string ResourcesPath = "Resources";
-
-        public const string AdminConfigurationKey = "AdminConfiguration";
     }
 }
