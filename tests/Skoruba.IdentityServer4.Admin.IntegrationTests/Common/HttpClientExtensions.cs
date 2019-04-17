9c89779a0d7ba9b1b2cd50639c6c1b6f1a059eea
Michał Drzał <michal.drzal@sybillatechnologies.com>
2019-04-18 00:48:32 +0200

    Simplified integration tests for admin server

--- tests/Skoruba.IdentityServer4.Admin.IntegrationTests/Common/HttpClientExtensions.cs
+++ tests/Skoruba.IdentityServer4.Admin.IntegrationTests/Common/HttpClientExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http;
+using System.Security.Claims;
 using Skoruba.IdentityServer4.Admin.Configuration.Constants;
 using Skoruba.IdentityServer4.Admin.Middlewares;
 
@@ -8,10 +10,16 @@ namespace Skoruba.IdentityServer4.Admin.IntegrationTests.Common
     {
         public static void SetAdminClaimsViaHeaders(this HttpClient client)
         {
-            client.DefaultRequestHeaders.Add($"{AuthenticatedTestRequestMiddleware.TestUserPrefixHeader}-{AuthenticatedTestRequestMiddleware.TestUserId}", "1");
-            client.DefaultRequestHeaders.Add($"{AuthenticatedTestRequestMiddleware.TestUserPrefixHeader}-{AuthenticatedTestRequestMiddleware.TestUserName}", "test");
-            client.DefaultRequestHeaders.Add($"{AuthenticatedTestRequestMiddleware.TestUserPrefixHeader}-{AuthenticatedTestRequestMiddleware.TestUserRoles}", AuthorizationConsts.AdministrationRole);
-        }
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, "1"),
+                new Claim(ClaimTypes.Name, "test"),
+                new Claim(ClaimTypes.Role, AuthorizationConsts.AdministrationRole)
+            };
 
+            var token = new JwtSecurityToken(claims: claims);
+            var t = new JwtSecurityTokenHandler().WriteToken(token);
+            client.DefaultRequestHeaders.Add(AuthenticatedTestRequestMiddleware.TestAuthorizationHeader, t);
+        }
     }
 }
