f3ebcce9bd60ab041753764e6e445063680ab04c
EUROPE\KULSER <sergii.kulyk@if.lv>
2019-06-11 19:34:42 +0300

    version 2 scaffolding https://dejanstojanovic.net/aspnet/2018/november/tracking-data-changes-with-entity-framework-core/
    (cherry picked from commit 125282c3b41f0fe1fad6349e404fa17260bea6ad)

--- src/Skoruba.IdentityServer4.Admin.EntityFramework.DbContexts/IdentityServerConfigurationDbContext.cs
+++ src/Skoruba.IdentityServer4.Admin.EntityFramework.DbContexts/IdentityServerConfigurationDbContext.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 using IdentityServer4.EntityFramework.DbContexts;
 using IdentityServer4.EntityFramework.Entities;
 using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Skoruba.IdentityServer4.Admin.EntityFramework.Interfaces;
 
@@ -211,10 +213,121 @@ namespace Skoruba.IdentityServer4.Admin.EntityFramework.DbContexts
         public string KeyValues { get; set; }
         public string OldValues { get; set; }
         public string NewValues { get; set; }
+        public String Username { get; set; }
+        public String Action { get; set; }
     }
 
     public class ClientConfigAudit : AuditBase
     {
 
+    }
+
+   // https://dejanstojanovic.net/aspnet/2018/november/tracking-data-changes-with-entity-framework-core/
+    public class CatalogDbContext : DbContext
+    {
+        private readonly ILoggerFactory loggerFactory;
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public CatalogDbContext(DbContextOptions<CatalogDbContext> options)
+         : base(options)
+        {
+        }
+
+        public CatalogDbContext(DbContextOptions<CatalogDbContext> options, ILoggerFactory loggerFactory, IHttpContextAccessor httpContextAccessor)
+          : base(options)
+        {
+            this.loggerFactory = loggerFactory;
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLoggerFactory(loggerFactory);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            //builder.ApplyConfiguration(new ProductConfiguration());
+        }
+
+        public override async Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var temoraryAuditEntities = await AuditNonTemporaryProperties();
+            var result = await base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+            await AuditTemporaryProperties(temoraryAuditEntities);
+            return result;
+        }
+
+
+        async Task<IEnumerable<Tuple<EntityEntry, ClientConfigAudit>>> AuditNonTemporaryProperties()
+        {
+            ChangeTracker.DetectChanges();
+            var entitiesToTrack = ChangeTracker.Entries().Where(e => !(e.Entity is ClientConfigAudit) && e.State != EntityState.Detached && e.State != EntityState.Unchanged);
+
+            await Audits.AddRangeAsync(
+                entitiesToTrack.Where(e => !e.Properties.Any(p => p.IsTemporary)).Select(e => new ClientConfigAudit()
+                {
+                    TableName = e.Metadata.Relational().TableName,
+                    Action = Enum.GetName(typeof(EntityState), e.State),
+                    DateTime = DateTime.Now.ToUniversalTime(),
+                    Username = this.httpContextAccessor?.HttpContext?.User?.Identity?.Name,
+                    KeyValues = JsonConvert.SerializeObject(e.Properties.Where(p => p.Metadata.IsPrimaryKey()).ToDictionary(p => p.Metadata.Name, p => p.CurrentValue).NullIfEmpty()),
+                    NewValues = JsonConvert.SerializeObject(e.Properties.Where(p => e.State == EntityState.Added || e.State == EntityState.Modified).ToDictionary(p => p.Metadata.Name, p => p.CurrentValue).NullIfEmpty()),
+                    OldValues = JsonConvert.SerializeObject(e.Properties.Where(p => e.State == EntityState.Deleted || e.State == EntityState.Modified).ToDictionary(p => p.Metadata.Name, p => p.OriginalValue).NullIfEmpty())
+                }).ToList()
+            );
+
+            //Return list of pairs of EntityEntry and ToolAudit  
+            return entitiesToTrack.Where(e => e.Properties.Any(p => p.IsTemporary))
+                 .Select(e => new Tuple<EntityEntry, ClientConfigAudit>(
+                     e,
+                 new ClientConfigAudit()
+                 {
+                     TableName = e.Metadata.Relational().TableName,
+                     Action = Enum.GetName(typeof(EntityState), e.State),
+                     DateTime = DateTime.Now.ToUniversalTime(),
+                     Username = this.httpContextAccessor?.HttpContext?.User?.Identity?.Name,
+                     NewValues = JsonConvert.SerializeObject(e.Properties.Where(p => !p.Metadata.IsPrimaryKey()).ToDictionary(p => p.Metadata.Name, p => p.CurrentValue).NullIfEmpty())
+                 }
+                 )).ToList();
+        }
+
+        async Task AuditTemporaryProperties(IEnumerable<Tuple<EntityEntry, ClientConfigAudit>> temporatyEntities)
+        {
+            if (temporatyEntities != null && temporatyEntities.Any())
+            {
+                await Audits.AddRangeAsync(
+                temporatyEntities.ForEach(t => t.Item2.KeyValues = JsonConvert.SerializeObject(t.Item1.Properties.Where(p => p.Metadata.IsPrimaryKey()).ToDictionary(p => p.Metadata.Name, p => p.CurrentValue).NullIfEmpty()))
+                    .Select(t => t.Item2)
+                );
+                await SaveChangesAsync();
+            }
+            await Task.CompletedTask;
+        }
+
+        //public DbSet<Product> Products { get; set; }
+        public DbSet<ClientConfigAudit> Audits { get; set; }
+    }
+
+    public static class Extensions
+    {
+        public static IDictionary<TKey, TValue> NullIfEmpty<TKey, TValue>(this IDictionary<TKey, TValue> dictionary)
+        {
+            if (dictionary == null || !dictionary.Any())
+            {
+                return null;
+            }
+            return dictionary;
+        }
+
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (T element in source)
+            {
+                action(element);
+            }
+            return source;
+        }
     }
 }
