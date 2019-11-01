17dc8a2ff9da1a223a490d8e2d29e4c45af660e5
janskoruba <jan@skoruba.com>
2019-11-01 13:20:27 +0100

    Add javascripts for audit logging and update js file for error logging.

---
+++ src/Skoruba.IdentityServer4.Admin/Scripts/App/pages/AuditLog.js
@@ -0,0 +1,54 @@
+﻿var auditLog = {
+
+    createJsonTree: function(json) {
+        var result;
+
+        try {
+            result = JSONTree.create(JSON.parse(json));
+        } catch (e) {
+            result = JSONTree.create(json);
+        }
+
+        return result;
+    },
+
+    initJsonTrees: function() {
+        $(".json-tree").each(function () {
+
+            var json = $(this).data("json-tree");
+            var result = auditLog.createJsonTree(json);
+
+            $(this).html(result);
+        });
+    },
+
+    eventHandlers: function() {
+        $(".audit-subject-button").click(function () {
+            var subjectId = $(this).data("subject-identifier");
+            var subjectType = $(this).data("subject-type");
+            var json = $(this).data("subject-additional-data");
+
+            $(".audit-modal-title").html(subjectId + " (" + subjectType + ")");
+            $(".audit-modal-value").html(auditLog.createJsonTree(json));
+            $(".audit-modal").modal("show");
+        });
+
+        $(".audit-action-button").click(function () {
+            var json = $(this).data("action");
+            $(".audit-modal-title").html("");
+            $(".audit-modal-value").html(auditLog.createJsonTree(json));
+            $(".audit-modal").modal("show");
+        });
+    },
+
+    init: function() {
+
+        $(function() {
+            auditLog.eventHandlers();
+            auditLog.initJsonTrees();
+        });
+
+    }
+};
+
+auditLog.init();
