3eca9fb504009adc598c13dc73be04a1e4bec22f
janskoruba <jan@skoruba.com>
2019-11-16 10:35:29 +0100

    polish migration script

--- build/add-migrations.ps1
+++ build/add-migrations.ps1
@@ -1,29 +1,27 @@
-param([string] $migrations = 'DbInit')
+param([string] $migration = 'DbInit')
 $currentPath = Get-Location
 Set-Location ../src/Skoruba.IdentityServer4.Admin
 
 Copy-Item appsettings.json -Destination appsettings-backup.json
 $settings = Get-Content appsettings.json -raw
 
-# SQL Server Migration
-$settings = $settings -replace '"ProviderType".*', '"ProviderType": "SqlServer",'
-$settings | set-content appsettings.json
+$dbProviders = @{ }
 
-dotnet ef migrations add $migrations -c AdminIdentityDbContext -o Migrations/Identity -p ..\..\src\Skoruba.IdentityServer4.Admin.EntityFramework.SqlServer\Skoruba.IdentityServer4.Admin.EntityFramework.SqlServer.csproj
-dotnet ef migrations add $migrations -c AdminLogDbContext -o Migrations/Logging -p ..\..\src\Skoruba.IdentityServer4.Admin.EntityFramework.SqlServer\Skoruba.IdentityServer4.Admin.EntityFramework.SqlServer.csproj
-dotnet ef migrations add $migrations -c IdentityServerConfigurationDbContext -o Migrations/IdentityServerConfiguration -p ..\..\src\Skoruba.IdentityServer4.Admin.EntityFramework.SqlServer\Skoruba.IdentityServer4.Admin.EntityFramework.SqlServer.csproj
-dotnet ef migrations add $migrations -c IdentityServerPersistedGrantDbContext -o Migrations/IdentityServerGrants -p ..\..\src\Skoruba.IdentityServer4.Admin.EntityFramework.SqlServer\Skoruba.IdentityServer4.Admin.EntityFramework.SqlServer.csproj
-dotnet ef migrations add $migrations -c AdminAuditLogDbContext -o Migrations/AuditLogging -p ..\..\src\Skoruba.IdentityServer4.Admin.EntityFramework.SqlServer\Skoruba.IdentityServer4.Admin.EntityFramework.SqlServer.csproj
+$dbProviders.Add("SqlServer", "..\..\src\Skoruba.IdentityServer4.Admin.EntityFramework.SqlServer\Skoruba.IdentityServer4.Admin.EntityFramework.SqlServer.csproj")
+$dbProviders.Add("PostgreSQL", "..\..\src\Skoruba.IdentityServer4.Admin.EntityFramework.PostgreSQL\Skoruba.IdentityServer4.Admin.EntityFramework.PostgreSQL.csproj")
 
-# Postgre Migration
-$settings = $settings -replace '"ProviderType".*', '"ProviderType": "PostgreSQL"'
-$settings | set-content appsettings.json
+foreach ($key in $dbProviders.Keys) {
+    Write-Host $dbProviders[$key]
 
-dotnet ef migrations add $migrations -c AdminIdentityDbContext -o Migrations/Identity -p ..\..\src\Skoruba.IdentityServer4.Admin.EntityFramework.PostgreSQL\Skoruba.IdentityServer4.Admin.EntityFramework.PostgreSQL.csproj
-dotnet ef migrations add $migrations -c AdminLogDbContext -o Migrations/Logging -p ..\..\src\Skoruba.IdentityServer4.Admin.EntityFramework.PostgreSQL\Skoruba.IdentityServer4.Admin.EntityFramework.PostgreSQL.csproj
-dotnet ef migrations add $migrations -c IdentityServerConfigurationDbContext -o Migrations/IdentityServerConfiguration -p ..\..\src\Skoruba.IdentityServer4.Admin.EntityFramework.PostgreSQL\Skoruba.IdentityServer4.Admin.EntityFramework.PostgreSQL.csproj
-dotnet ef migrations add $migrations -c IdentityServerPersistedGrantDbContext -o Migrations/IdentityServerGrants -p ..\..\src\Skoruba.IdentityServer4.Admin.EntityFramework.PostgreSQL\Skoruba.IdentityServer4.Admin.EntityFramework.PostgreSQL.csproj
-dotnet ef migrations add $migrations -c AdminAuditLogDbContext -o Migrations/AuditLogging -p ..\..\src\Skoruba.IdentityServer4.Admin.EntityFramework.PostgreSQL\Skoruba.IdentityServer4.Admin.EntityFramework.PostgreSQL.csproj
+    $settings = $settings -replace '"ProviderType".*', '"ProviderType": "' + $key + '",'
+    $settings | set-content appsettings.json
+
+    dotnet ef migrations add $migration -c AdminIdentityDbContext -o Migrations/Identity -p $dbProviders[$key]
+    dotnet ef migrations add $migration -c AdminLogDbContext -o Migrations/Logging -p $dbProviders[$key]
+    dotnet ef migrations add $migration -c IdentityServerConfigurationDbContext -o Migrations/IdentityServerConfiguration -p $dbProviders[$key]
+    dotnet ef migrations add $migration -c IdentityServerPersistedGrantDbContext -o Migrations/IdentityServerGrants -p $dbProviders[$key]
+    dotnet ef migrations add $migration -c AdminAuditLogDbContext -o Migrations/AuditLogging -p $dbProviders[$key]
+}
 
 Remove-Item appsettings.json
 Copy-Item appsettings-backup.json -Destination appsettings.json
