c651eebf89eb66008b91907e036fd9f5f8566482
janskoruba <jan@skoruba.com>
2020-01-01 11:52:14 +0100

    Update to .Net Core 3.1 and update template

---
+++ templates/2-publish-template.ps1
@@ -0,0 +1,7 @@
+$templateNuspecPath = "template-publish/Skoruba.IdentityServer4.Admin.Templates.nuspec"
+nuget pack $templateNuspecPath
+
+$templateLocalName = "Skoruba.IdentityServer4.Admin.Templates.1.0.0-beta8.nupkg"
+dotnet.exe new -i $templateLocalName
+
+dotnet.exe new skoruba.is4admin --name MyProject --title MyProject --adminrole MyRole --adminclientid MyClientId --adminclientsecret MyClientSecret --dockersupport true
