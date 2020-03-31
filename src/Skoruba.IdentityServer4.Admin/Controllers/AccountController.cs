74d47026ebff64969f96d8e3caad0a4495652f7f
j1shin <cradle-of-mail@gmx.de>
2020-03-31 16:15:43 +0200

    - IdentityServer4.Admin, Account/AccessDenied, replaced ViewData in favor of accessing RootConfiguration directly via injected interface, refs issue #552

--- src/Skoruba.IdentityServer4.Admin/Controllers/AccountController.cs
+++ src/Skoruba.IdentityServer4.Admin/Controllers/AccountController.cs
@@ -3,26 +3,19 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Skoruba.IdentityServer4.Admin.Configuration.Constants;
-using Skoruba.IdentityServer4.Admin.Configuration.Interfaces;
+using IdentityServer4.Admin.Configuration.Constants;
 
-namespace Skoruba.IdentityServer4.Admin.Controllers
+namespace IdentityServer4.Admin.Controllers
 {
     [Authorize]
     public class AccountController : BaseController
     {
-        private readonly IRootConfiguration _rootConfiguration;
-
-        public AccountController(ILogger<ConfigurationController> logger, IRootConfiguration rootConfiguration) : base(logger)
+        public AccountController(ILogger<ConfigurationController> logger) : base(logger)
         {
-            _rootConfiguration = rootConfiguration;
         }
 
         public IActionResult AccessDenied()
         {
-            ViewData["IdentityServerBaseUrl"] = _rootConfiguration.AdminConfiguration.IdentityServerBaseUrl;
-            ViewData["FaviconUri"] = _rootConfiguration.AdminConfiguration.FaviconUri;
-
             return View();
         }
 
