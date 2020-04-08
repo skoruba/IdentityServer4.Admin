62602c3cc6d2b12f107a7af9bf2186d63c2a16ae
j1shin <cradle-of-mail@gmx.de>
2020-04-08 09:22:37 +0200

    - IdentityServer4.Admin, fixed merge issues, refs issue #552

--- src/Skoruba.IdentityServer4.Admin/Controllers/AccountController.cs
+++ src/Skoruba.IdentityServer4.Admin/Controllers/AccountController.cs
@@ -1,11 +1,10 @@
-﻿using System.Collections.Generic;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using IdentityServer4.Admin.Configuration.Constants;
+using Skoruba.IdentityServer4.Admin.Configuration.Constants;
+using System.Collections.Generic;
 
-namespace IdentityServer4.Admin.Controllers
+namespace Skoruba.IdentityServer4.Admin.Controllers
 {
     [Authorize]
     public class AccountController : BaseController
