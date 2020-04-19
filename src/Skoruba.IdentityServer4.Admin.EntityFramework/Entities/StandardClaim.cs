4f5dc4460f53333e994611fae76ac6ab294d6754
Inbar Barkai <inbarb@nanolocksec.com>
2020-04-19 15:47:17 +0300

    Added StandardClaim entity.

---
+++ src/Skoruba.IdentityServer4.Admin.EntityFramework/Entities/StandardClaim.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Skoruba.IdentityServer4.Admin.EntityFramework.Entities
+{
+    public class StandardClaim
+    {
+        public int Id { get; set; }
+
+        public string ClaimType { get; set; }
+
+        public DateTimeOffset? LastUsedTimestamp { get; set; }
+
+        public long UseCount { get; set; }
+    }
+}
