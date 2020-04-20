8e499553680435e6b5ed2a79441ab5cd63fa7535
Inbar Barkai <inbarb@nanolocksec.com>
2020-04-20 06:39:00 +0300

    Create separate migrations for standard claims for PostgreSql.

---
+++ src/Skoruba.IdentityServer4.Admin.EntityFramework.PostgreSQL/Migrations/IdentityServerConfiguration/20200420033718_StandardScopes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Skoruba.IdentityServer4.Admin.EntityFramework.PostgreSQL.Migrations.IdentityServerConfiguration
+{
+    public partial class StandardScopes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StandardClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ClaimType = table.Column<string>(nullable: true),
+                    LastUsedTimestamp = table.Column<DateTimeOffset>(nullable: true),
+                    UseCount = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StandardClaims", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StandardClaims_ClaimType",
+                table: "StandardClaims",
+                column: "ClaimType",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StandardClaims");
+        }
+    }
+}
