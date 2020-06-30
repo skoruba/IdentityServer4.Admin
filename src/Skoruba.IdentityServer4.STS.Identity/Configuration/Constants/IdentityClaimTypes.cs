bbc880bb8166a035ce6203c5fee85316f2f8218d
Mehmet Perk <perkmehmed@gmail.com>
2020-06-30 11:43:37 +0300

    identityserver4 multi tenant integration

---
+++ src/Skoruba.IdentityServer4.STS.Identity/Configuration/Constants/IdentityClaimTypes.cs
@@ -0,0 +1,7 @@
+﻿namespace Skoruba.IdentityServer4.STS.Identity.Configuration.Constants
+{
+    public static class IdentityClaimTypes
+    {
+        public static readonly string TenantId = "tenantId";
+    }
+}
---
+++ src/Skoruba.IdentityServer4.STS.Identity/Services/IdentityClaimsService.cs
@@ -0,0 +1,29 @@
+﻿using IdentityServer4.Services;
+using Microsoft.Extensions.Logging;
+using Skoruba.IdentityServer4.STS.Identity.Configuration.Constants;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+namespace Skoruba.IdentityServer4.STS.Identity.Services
+{
+    public class IdentityClaimsService : DefaultClaimsService
+    {
+        public IdentityClaimsService(IProfileService profile, ILogger<IdentityClaimsService> logger)
+            : base(profile, logger)
+        {
+
+        }
+
+        protected override IEnumerable<Claim> GetOptionalClaims(ClaimsPrincipal subject)
+        {
+            var tenantClaim = subject.FindFirst(IdentityClaimTypes.TenantId);
+            if (tenantClaim == null)
+            {
+                return base.GetOptionalClaims(subject);
+            }
+
+            return base.GetOptionalClaims(subject).Union(new[] { tenantClaim });
+        }
+    }
+}
