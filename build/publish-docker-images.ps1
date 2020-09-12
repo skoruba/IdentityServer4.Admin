c8b9a2d0a5530c09687afd842651e02d3de27428
janskoruba <jan@skoruba.com>
2020-09-12 10:04:42 +0200

    Update publish-docker-images.ps1

--- build/publish-docker-images.ps1
+++ build/publish-docker-images.ps1
@@ -2,11 +2,11 @@
 docker-compose build
 
 # rename images with following tag
-docker tag skoruba-identityserver4-admin skoruba/identityserver4-admin:rc1
-docker tag skoruba-identityserver4-sts-identity skoruba/identityserver4-sts-identity:rc1
-docker tag skoruba-identityserver4-admin-api skoruba/identityserver4-admin-api:rc1
+docker tag skoruba-identityserver4-admin skoruba/identityserver4-admin:rc2
+docker tag skoruba-identityserver4-sts-identity skoruba/identityserver4-sts-identity:rc2
+docker tag skoruba-identityserver4-admin-api skoruba/identityserver4-admin-api:rc2
 
 # push to docker hub
-docker push skoruba/identityserver4-admin:rc1
-docker push skoruba/identityserver4-admin-api:rc1
-docker push skoruba/identityserver4-sts-identity:rc1
+docker push skoruba/identityserver4-admin:rc2
+docker push skoruba/identityserver4-admin-api:rc2
+docker push skoruba/identityserver4-sts-identity:rc2
