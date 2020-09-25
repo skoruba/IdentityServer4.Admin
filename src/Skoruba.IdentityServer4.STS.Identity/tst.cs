8afaf54a5a320211a43b87bd77f7fce4ba149438
Svyatukha Artyom <svyatukha@gmail.com>
2020-09-25 10:01:46 +0300

    tst

---
+++ src/Skoruba.IdentityServer4.STS.Identity/tst.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Skoruba.IdentityServer4.STS.Identity
+{
+    public class tst
+    {
+    }
+}
