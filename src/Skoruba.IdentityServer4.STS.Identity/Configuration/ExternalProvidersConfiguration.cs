1c4c3b9522e511ae721eb7d9d967085b64120f4a
Simo Paasisalo <simo.paasisalo@env.fi>
2020-10-21 20:53:54 +0300

    cleanup

--- src/Skoruba.IdentityServer4.STS.Identity/Configuration/ExternalProvidersConfiguration.cs
+++ src/Skoruba.IdentityServer4.STS.Identity/Configuration/ExternalProvidersConfiguration.cs
@@ -5,6 +5,7 @@
         public bool UseGitHubProvider { get; set; }
         public string GitHubClientId { get; set; }
         public string GitHubClientSecret { get; set; }
+
         public bool UseAzureAdProvider { get; set; }
         public string AzureAdClientId { get; set; }
         public string AzureAdSecret { get; set; }
@@ -12,6 +13,7 @@
         public string AzureInstance { get; set; }
         public string AzureAdCallbackPath { get; set; }
         public string AzureDomain { get; set; }
+        
         public bool UseSaml2Provider { get; set; }
         public string Saml2OurEntityId { get; set; }
         public string Saml2TheirEntityId { get; set; }
