be80b86ad60a3ce38768d7945e5e615fc3044a31
Brice <brice@xcit.org>
2021-01-06 15:45:04 +0100

    Moved UI folders and files to new Admin.UI project

---
+++ src/Skoruba.IdentityServer4.Admin.UI/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Skoruba.IdentityServer4.Admin.Configuration.Constants;
+using System.Collections.Generic;
+
+namespace Skoruba.IdentityServer4.Admin.Controllers
+{
+    [Authorize]
+    public class AccountController : BaseController
+    {
+        public AccountController(ILogger<ConfigurationController> logger) : base(logger)
+        {
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return new SignOutResult(new List<string> { AuthenticationConsts.SignInScheme, AuthenticationConsts.OidcAuthenticationScheme });
+        }
+    }
+}
