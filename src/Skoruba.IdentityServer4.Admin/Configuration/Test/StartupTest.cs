0d54be133c33ae815e237dedc2173cbe71d213d0
Brice <brice@xcit.org>
2021-01-08 11:50:18 +0100

    Cleaned up startup test

--- src/Skoruba.IdentityServer4.Admin/Configuration/Test/StartupTest.cs
+++ src/Skoruba.IdentityServer4.Admin/Configuration/Test/StartupTest.cs
@@ -19,27 +19,5 @@ namespace Skoruba.IdentityServer4.Admin.Configuration.Test
             // Use staging DbContexts and auth services.
             options.IsStaging = true;
         }
-
-        //public override void RegisterDbContexts(IServiceCollection services)
-        //{
-        //    services.RegisterDbContextsStaging<AdminIdentityDbContext, IdentityServerConfigurationDbContext, IdentityServerPersistedGrantDbContext, AdminLogDbContext, AdminAuditLogDbContext, IdentityServerDataProtectionDbContext>();
-        //}
-
-        //public override void RegisterAuthentication(IServiceCollection services)
-        //{
-        //    services.AddAuthenticationServicesStaging<AdminIdentityDbContext, UserIdentity, UserIdentityRole>();
-        //}
-
-        //public override void RegisterAuthorization(IServiceCollection services)
-        //{
-        //    var rootConfiguration = CreateRootConfiguration();
-        //    services.AddAuthorizationPolicies(rootConfiguration);
-        //}
-
-        //public override void UseAuthentication(IApplicationBuilder app)
-        //{
-        //    app.UseAuthentication();
-        //    app.UseMiddleware<AuthenticatedTestRequestMiddleware>();
-        //}
     }
 }
