e43877ab8da70ffcc47d3ff4f15b2640767597f5
Brice <brice@xcit.org>
2021-01-08 11:42:56 +0100

    Cleaned up startup

--- src/Skoruba.IdentityServer4.Admin/Startup.cs
+++ src/Skoruba.IdentityServer4.Admin/Startup.cs
@@ -37,53 +37,8 @@ namespace Skoruba.IdentityServer4.Admin
             // Adds the IdentityServer4 Admin UI with custom options.
             services.AddIdentityServer4AdminUI(ConfigureUIOptions);
 
-            //// Add DbContexts for Asp.Net Core Identity, Logging and IdentityServer - Configuration store and Operational store
-            //RegisterDbContexts(services);
-
-            //// Save data protection keys to db, using a common application name shared between Admin and STS
-            //services.AddDataProtection<IdentityServerDataProtectionDbContext>(Configuration);
-
             // Add email senders which is currently setup for SendGrid and SMTP
             services.AddEmailSenders(Configuration);
-
-            //// Add Asp.Net Core Identity Configuration and OpenIdConnect auth as well
-            //RegisterAuthentication(services);
-
-            //// Add HSTS options
-            //RegisterHstsOptions(services);
-
-            //// Add exception filters in MVC
-            //services.AddMvcExceptionFilters();
-
-            //// Add all dependencies for IdentityServer Admin
-            //services.AddAdminServices<IdentityServerConfigurationDbContext, IdentityServerPersistedGrantDbContext, AdminLogDbContext>();
-
-            //// Add all dependencies for Asp.Net Core Identity
-            //// If you want to change primary keys or use another db model for Asp.Net Core Identity:
-            //services.AddAdminAspNetIdentityServices<AdminIdentityDbContext, IdentityServerPersistedGrantDbContext,
-            //    IdentityUserDto, IdentityRoleDto, UserIdentity, UserIdentityRole, string, UserIdentityUserClaim, UserIdentityUserRole,
-            //                    UserIdentityUserLogin, UserIdentityRoleClaim, UserIdentityUserToken,
-            //                    IdentityUsersDto, IdentityRolesDto, IdentityUserRolesDto,
-            //                    IdentityUserClaimsDto, IdentityUserProviderDto, IdentityUserProvidersDto, IdentityUserChangePasswordDto,
-            //                    IdentityRoleClaimsDto, IdentityUserClaimDto, IdentityRoleClaimDto>();
-
-            //// Add all dependencies for Asp.Net Core Identity in MVC - these dependencies are injected into generic Controllers
-            //// Including settings for MVC and Localization
-            //// If you want to change primary keys or use another db model for Asp.Net Core Identity:
-            //services.AddMvcWithLocalization<IdentityUserDto, IdentityRoleDto,
-            //    UserIdentity, UserIdentityRole, string, UserIdentityUserClaim, UserIdentityUserRole,
-            //    UserIdentityUserLogin, UserIdentityRoleClaim, UserIdentityUserToken,
-            //    IdentityUsersDto, IdentityRolesDto, IdentityUserRolesDto,
-            //    IdentityUserClaimsDto, IdentityUserProviderDto, IdentityUserProvidersDto, IdentityUserChangePasswordDto,
-            //    IdentityRoleClaimsDto, IdentityUserClaimDto, IdentityRoleClaimDto>(Configuration);
-
-            //// Add authorization policies for MVC
-            //RegisterAuthorization(services);
-
-            //// Add audit logging
-            //services.AddAuditEventLogging<AdminAuditLogDbContext, AuditLog>(Configuration);
-
-            //services.AddIdSHealthChecks<IdentityServerConfigurationDbContext, IdentityServerPersistedGrantDbContext, AdminIdentityDbContext, AdminLogDbContext, AdminAuditLogDbContext, IdentityServerDataProtectionDbContext>(Configuration, rootConfiguration.AdminConfiguration);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
