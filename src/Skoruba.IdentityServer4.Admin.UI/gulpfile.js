91f1011742134808c3149cb9a2e380b49d3820f0
Jan Škoruba <jan@skoruba.com>
2021-02-13 13:20:40 +0100

    Sync PRs 772, 759 (#798)
    * Update README
    
    * Dutch translations added to resource (resx) files. (#759)
    
    * QR code white frame (#772)
    
    * Make gulp default task
    
    Fonts are needed in the styles so make them a dependency
    Also make gulp command run gulp build
    
    * Makes sure QR code can be scanned on a dark background
    
    Co-authored-by: timmytimeless <timmytimeless@hotmail.com>
    Co-authored-by: Jay Asbury <vbjay.net@gmail.com>

--- src/Skoruba.IdentityServer4.Admin.UI/gulpfile.js
+++ src/Skoruba.IdentityServer4.Admin.UI/gulpfile.js
@@ -1,4 +1,4 @@
-﻿var gulp = require('gulp');
+var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
@@ -99,7 +99,7 @@ function processTheme() {
         .pipe(gulp.dest(cssThemeFolder));
 }
 
-var buildStyles = gulp.series(processStyles, processTheme, processSass, processSassMin);
+var buildStyles = gulp.series(processFonts, processStyles, processTheme, processSass, processSassMin);
 var build = gulp.parallel(buildStyles, processScripts);
 
 gulp.task('clean', processClean);
@@ -109,6 +109,7 @@ gulp.task('sass:min', processSassMin);
 gulp.task('fonts', processFonts);
 gulp.task('scripts', processScripts);
 gulp.task('build', build);
+gulp.task('default', build);
 
 // watch
 function processWatch() {
