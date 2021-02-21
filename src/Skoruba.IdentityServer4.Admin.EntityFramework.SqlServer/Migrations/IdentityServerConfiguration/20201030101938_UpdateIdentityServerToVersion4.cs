43b401407e13efbf8869d134f962ed661917d38d
janskoruba <jan@skoruba.com>
2021-02-21 13:31:02 +0100

    Update db migration with source link

--- src/Skoruba.IdentityServer4.Admin.EntityFramework.SqlServer/Migrations/IdentityServerConfiguration/20201030101938_UpdateIdentityServerToVersion4.cs
+++ src/Skoruba.IdentityServer4.Admin.EntityFramework.SqlServer/Migrations/IdentityServerConfiguration/20201030101938_UpdateIdentityServerToVersion4.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Original SQL scripts for database migration come from: https://github.com/RockSolidKnowledge/IdentityServer4.Migration.Scripts/blob/master/MSSQL/ConfigurationDbContext.sql
+// Modified by Jan Škoruba
+
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Skoruba.IdentityServer4.Admin.EntityFramework.SqlServer.Migrations.IdentityServerConfiguration
