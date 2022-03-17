1a9567f02a23939de909d6af285431ef5c2a3dcf
Christopher Billingsley <cbillingsley@mindfiretechnology.com>
2022-03-17 12:38:56 -0600

    PR feedback - I guess we'll just commit the env files.

--- build-docker-mfire-stage.cmd
+++ build-docker-mfire-stage.cmd
@@ -1,5 +1,7 @@
 docker-compose -f docker-compose.yml --env-file stage.env -p is4-admin build
 
+timeout /t 10
+
 docker push docker.dev.mfire.co/identityserver4-admin:it
 docker push docker.dev.mfire.co/identityserver4-admin-api:it
 docker push docker.dev.mfire.co/identityserver4-sts-identity:it
--- build-docker-prod.cmd
+++ build-docker-prod.cmd
@@ -1,5 +1,7 @@
 docker-compose -f docker-compose.yml -p is4-admin build
 
+timeout /t 10
+
 docker push docker.dev.mfire.co/identityserver4-admin:latest
 docker push docker.dev.mfire.co/identityserver4-admin-api:latest
 docker push docker.dev.mfire.co/identityserver4-sts-identity:latest
